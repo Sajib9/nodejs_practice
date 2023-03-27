ec14bdd81d53eec92507c59b3b32f43084827458
sajib9 <sajib9@yahoo.com>
2023-03-27 14:26:18 +0600

    all commit

--- node_events/mongo_chat_project/app.js
+++ node_events/mongo_chat_project/app.js
@@ -1,41 +1,67 @@
-//external imports
+// external imports
 const express = require("express");
+const http = require("http");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const moment = require("moment");
 
-//internal imports
-const {notFoundHandler, errorHandler} = require("./middlewares/common/errorHandler");
+// internal imports
+const loginRouter = require("./router/loginRouter");
+const usersRouter = require("./router/usersRouter");
+const inboxRouter = require("./router/inboxRouter");
+
+// internal imports
+const {
+  notFoundHandler,
+  errorHandler,
+} = require("./middlewares/common/errorHandler");
 
 const app = express();
-dotenv.config(); 
+const server = http.createServer(app);
+dotenv.config();
 
-//database connection
-mongoose.connect(process.env.MONGO_CONNECTION_STRING )
-.then(()=>console.log("connection successful"))
-.catch(err=>console.log(err));
+// socket creation
+const io = require("socket.io")(server);
+global.io = io;
 
-//request parser
+// set comment as app locals
+app.locals.moment = moment;
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connection successful!"))
+  .catch((err) => console.log(err));
+
+// request parsers
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); //extended:true for query string
+app.use(express.urlencoded({ extended: true }));
 
-//set view engine
+// set view engine
 app.set("view engine", "ejs");
 
-//set static folder
-app.use(express.static(path.join(__dirname,"public")));
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-//parse cooies
+// parse cookies
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//routing setup
+// routing setup
+app.use("/", loginRouter);
+app.use("/users", usersRouter);
+app.use("/inbox", inboxRouter);
 
-//404 not found
+// 404 not found handler
 app.use(notFoundHandler);
-//common handler
+
+// common error handler
 app.use(errorHandler);
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`app listeing to port ${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
 });
--- node_events/mongo_chat_project/middlewares/common/errorHandler.js
+++ node_events/mongo_chat_project/middlewares/common/errorHandler.js
@@ -6,9 +6,19 @@ function notFoundHandler(req,res,next){
 
 //default error handler
 function errorHandler(err,req,res,next){
-    res.render('error',{
-        title:"Error page"
-    }); //view page name
+    res.locals.error = process.env.NODE_ENV === "development" ? err : {message : err.message};
+
+    res.status(err.status || 500);
+
+    if(res.locals.html){
+        res.render('error',{
+            title:"Error page"
+        }); //view page name
+    }else{
+        res.json(res.locals.error);
+    }
+
+    
 }
 
 module.exports = {
